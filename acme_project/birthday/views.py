84a3d84b48067910f90431d51cbe96beb360207b
Oleg Ritum <oritum@gmail.com>
2024-09-27 13:31:45 +0100

    1

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .forms import BirthdayForm
 
 
 def birthday(request):
-    context = {}
+    form = BirthdayForm(request.GET or None)
+    if form.is_valid():
+        pass
+    context = {'form': form}
     return render(request, 'birthday/birthday.html', context=context)
